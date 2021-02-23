a4e0dae2a7cd26bea6ee79653c9848c240cc9b2b
chasereske <chasereske@gmail.com>
2021-02-22 22:07:29 -0500

    Added the functionality to retrieve all the songs.

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ app.listen(3000, function() {
     console.log("Server started. Listening on port 3000.");
 });
 
+app.get('/api/songs', (req,res) => {
+    const songs = repoContext.songs.findAllSongs();
+    return res.send(songs);
+});
